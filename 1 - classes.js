efa19d782c2171ccede0ff26c8d9cca3445b04b5
Alex Key <alexander.key@gmail.com>
2016-04-19 18:45:07 +0100

    moved class definition

--- 1 - classes.js
+++ 1 - classes.js
@@ -1,17 +1,43 @@
 "use strict";
 
-class Person
-{
-  get Name() { return this.name };
-  get Age() { return this.age };
+require("chai").should();
 
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
+describe("classes", function() {
+  
+  class Person
+  {
+    get Name() { return this.name };
+    set FavouriteColour(value) { this.favouriteColour = value; }
+    get FavouriteColour() { return this.favouriteColour; }
+
+    constructor(name, age) {
+      this.name = name; //just uses this to set a dyanmic property.
+      this.age = age;
+    };
   }
-}
 
-var bob = new Person("Bob", 30);
+  let person;
 
-console.assert(bob.Name == "Bob");
-console.assert(bob.Age == 30);
+  beforeEach(function() {
+    person = new Person("Alex", 31);
+  })
+
+  it("can use a constructor without backing fields", function() {
+    person.Name.should.equal("Alex");
+  });
+
+  it("leaks fields", function() {
+    person.age.should.equal(31);
+  });
+
+  it("has setters", function() {
+    person.FavouriteColour = "Green";
+    person.FavouriteColour.should.equal("Green");
+  });
+
+  it("has dynamic properties", function() {
+    var propertyName = "na" + "me";
+    person[propertyName].should.equal("Alex");
+  });
+
+});
