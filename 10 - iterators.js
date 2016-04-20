9f0d4c9c9a4f664dcce757a0f13ef55476ea04fb
Alex Key <alexander.key@gmail.com>
2016-04-20 21:20:27 +0100

    added usage example of iterators

--- 10 - iterators.js
+++ 10 - iterators.js
@@ -2,6 +2,8 @@
 
 require("chai").should();
 
+//mocha --harmony_rest_parameters
+
 describe("iterators", function() {
 
   var iteratorFunction = function() {
@@ -23,11 +25,57 @@ describe("iterators", function() {
 
   describe("iterables", function() {
     var iterable = {
-      [Symbol.iterator] : iteratorFunction()
+      [Symbol.iterator] : iteratorFunction
     };
 
     it("should have a iterator defined with the key Symbol.iterator", function() {
       iterable.should.have.a.property(Symbol.iterator);
+    });
+
+    it("is used by for in loops", function(){
+
+        class MovieGenre  {
+
+          constructor(name, ...movies) {
+            this.name = name;
+            this.movies = movies;
+          }
+
+          get Name() {
+            return this.name;
+          }
+
+          [Symbol.iterator]() {
+            let counter = 0;
+
+            //you can't have arrow functions in classes (yet - ES7) http://stackoverflow.com/a/31362350/141022
+            //so you need to grab a reference to the class properties.
+            let movies = this.movies;
+
+            return { //created a closure, so has access to counter
+              next : () => {
+                let done = counter >= movies.length;
+                let movie = movies[counter];
+
+                counter++;
+
+                return { done, value: movie };
+              }
+            }
+          }
+        }
+
+        let action = new MovieGenre("action", "Die Hard", "Die Harder", "Die Hardest");
+
+        let counter = 1;
+
+        for(let movie of action) {
+          if(counter === 3) {
+            movie.should.be.equal("Die Hardest");
+          }
+          counter++;
+        }
+
     });
 
   });
