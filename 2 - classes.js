efab0eaa8ff8910d654d6b55069c9b6b09c342fc
Alex Key <alexander.key@gmail.com>
2016-04-20 21:56:19 +0100

    added commments to classes

--- 2 - classes.js
+++ 2 - classes.js
@@ -3,12 +3,17 @@
 require("chai").should();
 
 describe("classes", function() {
-  
-  class Person
-  {
+
+  let sound = "dog";
+
+  class Person { //clases are not hoisted, need declaring before using.
+    //let eyeColor //you can't declare variables and no need for ,
+
+
     get Name() { return this.name };
     set FavouriteColour(value) { this.favouriteColour = value; }
     get FavouriteColour() { return this.favouriteColour; }
+    get [sound]() { return "woof"; }
 
     constructor(name, age) {
       this.name = name; //just uses this to set a dyanmic property.
@@ -22,7 +27,7 @@ describe("classes", function() {
     person = new Person("Alex", 31);
   })
 
-  it("can use a constructor without backing fields", function() {
+  it("can use a constructor without backing fields (just using this to create dynamic fields)", function() {
     person.Name.should.equal("Alex");
   });
 
@@ -35,9 +40,17 @@ describe("classes", function() {
     person.FavouriteColour.should.equal("Green");
   });
 
-  it("has dynamic properties", function() {
-    var propertyName = "na" + "me";
-    person[propertyName].should.equal("Alex");
+  it("has dynamic properties during definition and access!", function() {
+    var propertyName = "dog"
+    person[propertyName].should.equal("woof");
   });
+
+  it("can be extended", function() {
+
+    class Hero extends Person {
+
+    }
+
+  })
 
 });
